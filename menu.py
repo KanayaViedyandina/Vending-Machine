c63001a8b28e3d70158daed2fb40c226b5da4de1
KanayaViedyandina <117056997+KanayaViedyandina@users.noreply.github.com>
2022-10-31 20:37:42 +0700

    Create menu.py

---
+++ menu.py
@@ -0,0 +1,3 @@
+harga = [5000, 5000, 5000, 5000, 5000, 5000, 10000, 10000, 10000, 10000, 10000, 15000, 15000, 15000, 15000]
+menu = ["Aqua","Pucuk Harum","Mizone","Fanta","Sprite","Pepsi","Ichi Ocha","Frestea","Buavita","Floridina","Coolant","Joy Tea","Tebs","Pocari Sweat","Teh Botol Sosro"] 
+item_id = [i for i in range(15)]
